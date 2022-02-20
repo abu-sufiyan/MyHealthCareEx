02c6e719b61c7dfda9c367349a3e5b6ac0ac48b1
Abu Sufiyan <sufiyansrs@gmail.com>
2022-02-20 18:05:34 +0530

    first commit

---
+++ SpringBootHerokuTest/src/test/java/in/ras/rafey/SpringBootHerokuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ras.rafey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
